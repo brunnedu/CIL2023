3c57f8b54fdf4d40c0eca5da1a24a5a91dd39797
brunnedu <dustin.brunner@gmx.ch>
2023-03-14 17:53:20 +0100

    add normalization and slightly adjust augmentations

--- src/dataset.py
+++ src/dataset.py
@@ -8,7 +8,7 @@ from torchvision.io import ImageReadMode
 
 from albumentations.core.composition import BaseCompose
 
-from src.transforms import AUG_TRANSFORM
+from src.transforms import AUG_TRANSFORM, NORMALIZATION_TRANSFORM
 
 
 class SatelliteDataset(Dataset):
@@ -20,6 +20,7 @@ class SatelliteDataset(Dataset):
             data_dir: str = 'data/training',
             add_data_dir: Optional[str] = None,
             aug_transform: Optional[BaseCompose] = AUG_TRANSFORM,
+            post_transform: torch.nn.Module = NORMALIZATION_TRANSFORM,
     ):
         """
         Parameters
@@ -31,13 +32,16 @@ class SatelliteDataset(Dataset):
             Directory where additional data is located. Has to contain two subdirectories "images" & "groundtruth".
             Both subdirectories should contain files with matching names.
         aug_transform
-            An albumentation transform that is applied to both the satellite image and the road mask .
+            An albumentation transform that is applied to both the satellite image and the road mask.
+        post_transform
+            A torchvision transform that is applied to all images after augmentation.
         """
         self.data_dir = data_dir
         self.add_data_dir = add_data_dir
         self.img_paths = sorted([os.path.join(self.data_dir, 'images', f) for f in os.listdir(os.path.join(self.data_dir, 'images'))])
         self.mask_paths = sorted([os.path.join(self.data_dir, 'groundtruth', f) for f in os.listdir(os.path.join(self.data_dir, 'groundtruth'))])
         self.aug_transform = aug_transform
+        self.post_transform = post_transform
 
         if self.add_data_dir:
             # add additional data
@@ -57,12 +61,17 @@ class SatelliteDataset(Dataset):
         img_path = self.img_paths[idx]
         mask_path = self.mask_paths[idx]
 
+        # read img and mask
         img = torchvision.io.read_image(img_path, mode=ImageReadMode.RGB)/255
         mask = torchvision.io.read_image(mask_path, mode=ImageReadMode.GRAY)/255
 
         if self.aug_transform is not None:
+            # augment img and mask together
             transformed = self.aug_transform(image=img.permute(1, 2, 0).numpy(), mask=mask.permute(1, 2, 0).numpy())
-            img, mask = torch.from_numpy(transformed['image']).permute(2, 0, 1), torch.from_numpy(
-                transformed['mask']).permute(2, 0, 1)
+            img = torch.from_numpy(transformed['image']).permute(2, 0, 1)
+            mask = torch.from_numpy(transformed['mask']).permute(2, 0, 1)
+
+        # normalize img
+        img = self.post_transform(img)
 
         return img, mask
--- src/transforms.py
+++ src/transforms.py
@@ -1,8 +1,13 @@
 import albumentations as A
+from torchvision.transforms import Normalize
 
 AUG_TRANSFORM = A.Compose([
     A.Resize(height=224, width=224),
     A.HorizontalFlip(p=0.5),
     A.VerticalFlip(p=0.5),
     A.RandomRotate90(p=0.5),
+    A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1),
 ])
+
+# normalization transform using the 2022 dataset means and stds
+NORMALIZATION_TRANSFORM = Normalize(mean=(0.5098, 0.5205, 0.5180), std=(0.2315, 0.2096, 0.1998))
