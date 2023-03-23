02b5514352ae34992d05997f7341a4ddb43321bc
brunnedu <dustin.brunner@gmx.ch>
2023-03-23 18:10:00 +0100

    fix formatting

--- src/transforms.py
+++ src/transforms.py
@@ -3,11 +3,11 @@ from torchvision.transforms import Compose, Resize, Normalize
 
 AUG_TRANSFORM = A.Compose([
     A.Resize(height=224, width=224),
-    #A.RandomCrop(height=224, width=224)
+    # A.RandomCrop(height=224, width=224)
     A.HorizontalFlip(p=0.5),
     A.VerticalFlip(p=0.5),
     A.RandomRotate90(p=0.5),
-    #A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1),
+    # A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=1),
 ])
 
 RUN_TRANSFORM = Compose([
