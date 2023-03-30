486014780f968998d1ee69c28d61ddd63a414b94
brunnedu <dustin.brunner@gmx.ch>
2023-03-30 20:24:36 +0200

    update config to work with pytorch lightning

--- config.py
+++ config.py
@@ -1,40 +1,48 @@
-from src.models.unet.unet import UNet
-from src.models.unet.unet_pp import UNetPP
-from src.models.unet.backbones import Resnet18Backbone
-from src.models.unet.blocks import UpBlock
-from src.metrics.continuous import DiceLoss, JaccardLoss, FocalLoss, BinaryF1Score
+from src.models import UNet, UNetPP, Resnet18Backbone, UpBlock
+from src.metrics import DiceLoss, JaccardLoss, FocalLoss, BinaryF1Score, PatchAccuracy
 from src.transforms import AUG_TRANSFORM
 
 from torch.optim import Adam
 
 TRAIN_CONFIG = {
     'experiment_id': 'train_test',  # should be changed for every run
-    'resume_from_checkpoint': False,
+    'resume_from_checkpoint': False,  # currently not working with pytorch lightning
     'dataset_kwargs': {
         'data_dir': 'data/training',
-        'add_data_dir': None,  # specify to use additional data
-        'hist_equalization': True,
+        'add_data_dir': 'data/data_2022',  # specify to use additional data
+        'hist_equalization': False,
         'aug_transform': AUG_TRANSFORM,
     },
-    'model_cls': UNet,
-    'backbone_cls': Resnet18Backbone,
-    'model_kwargs': {
-        'up_block_ctor': lambda ci: UpBlock(ci, up_mode='upconvgs'),
+    'model_config': {
+        'model_cls': UNet,
+        'backbone_cls': Resnet18Backbone,
+        'model_kwargs': {
+            'up_block_ctor': lambda ci: UpBlock(ci, up_mode='upconv'),
+        },
     },
-    'optimizer_cls': Adam,
-    'optimizer_kwargs': {
-        'lr': 1e-3,
-        'weight_decay': 0,
+    'pl_wrapper_kwargs': {
+        'loss_fn': FocalLoss(alpha=0.25, gamma=2.0, bce_reduction='none'),
+        'val_metrics': {
+            'acc': PatchAccuracy(patch_size=16, cutoff=0.25),  # should always use PatchAccuracy as accuracy function
+            'binaryf1score': BinaryF1Score(alpha=100.0),  # can add as many additional metrics as desired
+        },
+        'optimizer_cls': Adam,
+        'optimizer_kwargs': {
+            'lr': 1e-3,
+            'weight_decay': 0,
+        },
+        'lr_scheduler_cls': None,
+        'lr_scheduler_kwargs': None,
     },
-    'train_model_kwargs': {
-        'accuracy_fn': BinaryF1Score(alpha=100.0),  # OneMinusLossScore(FocalLoss())
-        'criterion': FocalLoss(alpha=0.25, gamma=2.0, bce_reduction='none'),
+    'pl_trainer_kwargs': {
+        'max_epochs': 10,
+        'log_every_n_steps': 50,
+    },
+    'train_pl_wrapper': {
         'val_frac': 0.1,
-        'num_epochs': 100,
         'batch_size': 64,
         'num_workers': 4,  # set to 0 if multiprocessing leads to issues
-        'log_frequency': 10,
         'seed': 0,
-        'save_models': True,
+        'save_checkpoints': True,
     }
 }
