2513d125b54dc46d41e6efefaadf7442ec2f0746
brunnedu <dustin.brunner@gmx.ch>
2023-04-05 15:44:19 +0200

    reformat models slightly

--- src/models/unet/unet.py
+++ src/models/unet/unet.py
@@ -1,4 +1,3 @@
-
 import torch.nn as nn
 
 import typing as t
@@ -7,21 +6,22 @@ from .backbones import ABackbone
 
 
 class UNet(nn.Module):
-    '''
+    """
         Basic UNet architecture
         Parameters:
             - backbone: the "leftmost" nodes of the UNet, defines the depth and the amount of channels per level
             - up_block_ctor: function that is fed the amount of channels and returns an up node of the UNet
             - final: the final layers of the UNet
-    '''
-    def __init__(self, backbone : ABackbone, 
-                 up_block_ctor : t.Callable[[int], nn.Module], 
-                 final : t.Optional[nn.Module] = None):
+    """
+
+    def __init__(self, backbone: ABackbone,
+                 up_block_ctor: t.Callable[[int], nn.Module],
+                 final: t.Optional[nn.Module] = None):
         super().__init__()
 
         # down nodes / encoder
         self.backbone = backbone
-        
+
         # layers
         channels = self.backbone.get_channels()
         ups = []
@@ -44,7 +44,7 @@ class UNet(nn.Module):
         outs, x = self.backbone(x)
 
         b = outs[-1]
-        for s,up in zip(reversed(outs[:-1]),reversed(self.ups)):
+        for s, up in zip(reversed(outs[:-1]), reversed(self.ups)):
             b = up(b, [s])
 
         return self.final(b)
