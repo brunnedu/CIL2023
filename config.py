8b5cd975b6f13624ca22c2bbb32dcb1e7340aa79
brunnedu <dustin.brunner@gmx.ch>
2023-04-07 11:43:12 +0200

    update config

--- config.py
+++ config.py
@@ -1,18 +1,16 @@
-import torch
-from pytorch_lightning.callbacks import EarlyStopping
-
 from src.models import UNet, UNetPP, Resnet18Backbone, UpBlock
-from src.metrics import DiceLoss, JaccardLoss, FocalLoss, BinaryF1Score, PatchAccuracy
+from src.metrics import DiceLoss, JaccardLoss, FocalLoss, BinaryF1Score, PatchAccuracy, PatchF1Score
 from src.transforms import AUG_TRANSFORM
 
 from torch.optim import Adam
+import torch
 
 TRAIN_CONFIG = {
     'experiment_id': 'test_run',  # should be changed for every run
-    'resume_from_checkpoint': False,  # currently not working with pytorch lightning
+    'resume_from_checkpoint': False,  # set full experiment id (including timestamp) to resume from checkpoint
     'dataset_kwargs': {
         'data_dir': 'data/training',
-        'add_data_dir': None,  # specify to use additional data
+        'add_data_dir': 'data/data_2022',  # specify to use additional data
         'hist_equalization': False,
         'aug_transform': AUG_TRANSFORM,
     },
@@ -26,8 +24,9 @@ TRAIN_CONFIG = {
     'pl_wrapper_kwargs': {
         'loss_fn': FocalLoss(alpha=0.25, gamma=2.0, bce_reduction='none'),
         'val_metrics': {
-            'acc': PatchAccuracy(patch_size=16, cutoff=0.25),  # should always use PatchAccuracy as accuracy function
+            'acc': PatchAccuracy(patch_size=16, cutoff=0.25),
             'binaryf1score': BinaryF1Score(alpha=100.0),  # can add as many additional metrics as desired
+            # 'patchf1score': PatchF1Score(),
         },
         'optimizer_cls': Adam,
         'optimizer_kwargs': {
@@ -36,7 +35,6 @@ TRAIN_CONFIG = {
         },
         'lr_scheduler_cls': torch.optim.lr_scheduler.ReduceLROnPlateau,
         'lr_scheduler_kwargs': {
-            'monitor': 'val_loss',
             'mode': 'min',
             'factor': 0.2,
             'patience': 10,
@@ -44,16 +42,13 @@ TRAIN_CONFIG = {
     },
     'pl_trainer_kwargs': {
         'max_epochs': 100,
-        'log_every_n_steps': 50,
-        'callbacks': [  # list of callbacks (callback_cls, callback_kwargs)
-            (EarlyStopping, {'monitor': 'val_acc', 'patience': 10, 'mode': 'max'}),
-        ]
+        'log_every_n_steps': 1,
     },
     'train_pl_wrapper_kwargs': {
         'val_frac': 0.1,
-        'batch_size': 64,
-        'num_workers_dl': 4,  # set to 0 if multiprocessing leads to issues
+        'batch_size': 32,
+        'num_workers_dl': 2,  # set to 0 if multiprocessing leads to issues
         'seed': 0,
-        'save_checkpoints': True,  # turn off to save storage space
+        'save_checkpoints': True,
     }
 }
