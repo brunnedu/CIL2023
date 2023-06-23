fixed maunet exploding gradients

config.py
@@ -24,8 +24,7 @@ TRAIN_CONFIG = {
         },
     },
     'pl_wrapper_kwargs': {
-        #'loss_fn': FocalLoss(alpha=0.25, gamma=2.0, bce_reduction='none'),
-        'loss_fn': nn.BCELoss(),
+        'loss_fn': FocalLoss(alpha=0.25, gamma=2.0, bce_reduction='none'),
         'val_metrics': {
             'acc': PatchAccuracy(patch_size=16, cutoff=0.25),
             'binaryf1score': BinaryF1Score(alpha=100.0),  # can add as many additional metrics as desired

src/models/unet/ma_unet.py
@@ -54,7 +54,7 @@ class MAUNet(nn.Module):
         ca = self.channel_attention(x)
         sa = self.spatial_attention(x)
 
-        x = torch.sum(torch.cat([ca, sa], dim=1), 1).unsqueeze(1) # sum fusion = sum over all channels
+        x = torch.mean(torch.cat([ca, sa], dim=1), 1).unsqueeze(1) # sum fusion = sum over all channels
         
         # reduce to input size
         x = F.interpolate(x, (H,W), mode='bilinear').reshape(B, 1, H, W)