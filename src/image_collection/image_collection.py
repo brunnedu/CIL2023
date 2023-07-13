c09eafad59fa4852ffef490d257f9106a49966b1
Michael Ungersböck <ungersboeck.michael@gmail.com>
2023-07-13 18:30:14 +0200

    Add gmaps digital signature

--- src/image_collection/image_collection.py
+++ src/image_collection/image_collection.py
@@ -1,3 +1,8 @@
+import base64
+import hashlib
+import hmac
+import time
+import urllib
 from typing import Tuple
 import json
 import io
@@ -9,15 +14,54 @@ from tqdm import tqdm
 import requests
 from PIL import Image
 from datetime import datetime
+import urllib.parse as urlparse
 
 base_url = "https://maps.googleapis.com/maps/api/staticmap"
 api_key = os.getenv("GMAPS_API_KEY")
+sign_key = os.getenv("GMAPS_SIGN_KEY")
 
 
 def coordinate_generator(n, bbox):
     """Returns n random coordinates within the bbox."""
     for i in range(n):
         yield i, np.random.uniform(bbox[1]["lat"], bbox[0]["lat"]), np.random.uniform(bbox[0]["lng"], bbox[1]["lng"])
+
+
+def sign_url(input_url=None, secret=None):
+    """ Sign a request URL with a URL signing secret.
+      Usage:
+      from urlsigner import sign_url
+      signed_url = sign_url(input_url=my_url, secret=SECRET)
+      Args:
+      input_url - The URL to sign
+      secret    - Your URL signing secret
+      Returns:
+      The signed request URL
+  """
+
+    if not input_url or not secret:
+        raise Exception("Both input_url and secret are required")
+
+    url = urlparse.urlparse(input_url)
+
+    # We only need to sign the path+query part of the string
+    url_to_sign = url.path + "?" + url.query
+
+    # Decode the private key into its binary format
+    # We need to decode the URL-encoded private key
+    decoded_key = base64.urlsafe_b64decode(secret)
+
+    # Create a signature using the private key and the URL-encoded
+    # string using HMAC SHA1. This signature will be binary.
+    signature = hmac.new(decoded_key, str.encode(url_to_sign), hashlib.sha1)
+
+    # Encode the binary signature into base64 for use within a URL
+    encoded_signature = base64.urlsafe_b64encode(signature.digest())
+
+    original_url = url.scheme + "://" + url.netloc + url.path + "?" + url.query
+
+    # Return signed URL
+    return original_url + "&signature=" + encoded_signature.decode()
 
 
 def process_image(lat: float, lng: float, roadmap=True, zoom=18, size=400) -> Tuple[Image.Image, str]:
@@ -37,7 +81,8 @@ def process_image(lat: float, lng: float, roadmap=True, zoom=18, size=400) -> Tu
 
     # Construct Google Maps URL
     # Roads are marked in red, all points of interest and labels removed
-    url = f"{base_url}?center={lat},{lng}&zoom={zoom}&size={actual_size}x{actual_size}" \
+
+    query = f"center={lat},{lng}&zoom={zoom}&size={actual_size}x{actual_size}" \
           f"&maptype={map_type}&key={api_key}" \
           "&style=feature:road|element:geometry|color:0xff0000" \
           "&style=feature:poi|visibility:off" \
@@ -45,7 +90,19 @@ def process_image(lat: float, lng: float, roadmap=True, zoom=18, size=400) -> Tu
           "&style=feature:administrative|visibility:off" \
           "&style=feature:all|element:labels|visibility:off"
 
-    response = requests.get(url)
+    query_url = urllib.parse.quote_plus(query, safe="=,&")
+    url = f"{base_url}?{query_url}"
+    url = sign_url(url, sign_key)
+
+    while True:
+        try:
+            response = requests.get(url, timeout=5)
+            break
+        except Exception as e:
+            print(e)
+            print("Retrying in 1 second...")
+            time.sleep(5)
+
     image = Image.open(io.BytesIO(response.content))
     # crop image and remove watermark
     image = image.crop((border_size, border_size, actual_size - border_size, actual_size - border_size))
@@ -79,7 +136,7 @@ def process_location(location, log_file, satellite_path, roadmap_path, starting_
         img_satellite, _ = process_image(lat, lng, roadmap=False)
         img_roadmap, roadmap_url = process_image(lat, lng, roadmap=True)
 
-        filename = f"{location['name']}_satimage_{i+starting_index}.png"
+        filename = f"{location['name']}_satimage_{i + starting_index}.png"
         img_satellite.save(os.path.join(satellite_path, filename))
         img_roadmap.save(os.path.join(roadmap_path, filename))
         log_file.write(f"{location['name']},{lat},{lng},{filename},{roadmap_url}\n")
@@ -97,6 +154,8 @@ def main(config_path, output_path, starting_index):
     # check if google maps api key is available
     if not api_key:
         print(f"Google Maps API Key in environment variable 'GMAPS_API_KEY' required!")
+    elif not sign_key:
+        print(f"Google Maps Digital Signature Secret in environment variable 'GMAPS_SIGN_KEY' required!")
     elif os.path.exists(output_path) and len(os.listdir(output_path)) != 0:
         print(f"Output directory {output_path} is not empty!")
     else:
