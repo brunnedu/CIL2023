2a22ab601c18f4f96b3d0355b301f25f8344e846
Yanick Zengaffinen <yanick.zengaffinen@gmail.com>
2023-07-15 21:06:56 +0200

    fix channel selection

--- src/run.py
+++ src/run.py
@@ -25,7 +25,7 @@ def predict(
     output = F.interpolate(output, original_size, mode='bilinear')
 
     if select_channel is not None:
-        output = output[select_channel]
+        output = output[:,select_channel]
 
     return output
 
